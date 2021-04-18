bbdfa1688e6c9b92c95d14ff99b5c6c3bdd6a4c6
Avery Wiehe <averyswiehe@gmail.com>
2021-04-18 03:41:46 -0400

    Update local_git_config.sh

--- local_git_config.sh
+++ local_git_config.sh
@@ -1,14 +1,8 @@
 #!bin/bash
 
 sudo apt install git -y
-sudo git init
+sudo git clone https://github.com/aswiehe/ScriptCanvas.git
 sudo git config --global user.name sauron
 sudo git config --global user.email sauron@mordor
-sudo git remote add origin https://github.com/aswiehe/ScriptCanvas.git
-sudo git pull origin master
-sudo git checkout master
-sudo git add *
-sudo git commit -m "A new local repo has been created using setup.sh and has set its origin to this remote repo"
-sudo git push origin master
 
 exit
