979bf4bc96e611807b4e80f80274b0a565c12874
sauron <sauron@mordor>
2021-04-18 17:37:04 -0400

    Partially tested script to save custom display changes to system file

--- scripts/display_config.sh
+++ scripts/display_config.sh
@@ -6,6 +6,6 @@ xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 
 xrandr --addmode Virtual1 1920x1080_60.00
 xrandr --output Virtual1 --mode 1920x1080_60.00
 
-sed '/^OLF_IFS=$IFS$/ \nxrandr i "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync\nxrandr --addmode Virtual1 1920x1080_60.00\nxrandr --output Virtual1 --mode 1920x1080\n' /etc/gdm3/Init/DefaultTwo
+sudo sed -i 's/OLD_IFS=$IFS/OLD_IFS=$IFS\nxrandr "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync\nxrandr --addmode Virtual1 1920x1080_60.00\nxrandr --output Virtual1 --mode 1920x1080\n/g' /etc/gdm3/Init/DefaultTwo
 
 exit
