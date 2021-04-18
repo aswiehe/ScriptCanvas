6a797adeb3fcb383837f9d73b57c06497f87dfa8
Avery Wiehe <averyswiehe@gmail.com>
2021-04-18 13:38:05 -0400

    Create angular_setup.sh
    First draft, untested

---
+++ scripts/angular_setup.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+read -p "Name of application: " application_name
+read -p "Name of folder for new application (suggested 'Application'): " application_folder_name
+
+cd ~
+mkdir $application_folder_name
+cd $application_folder_name
+
+sudo apt install npm
+npm install -g @angular/cli
+ng new $application_name
+ng serve --open
+
+exit
