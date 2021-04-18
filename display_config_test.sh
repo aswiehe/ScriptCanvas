f83d0c73d59a9e1d069ffdd7b274ee55b0f8f9c4
Avery Wiehe <averyswiehe@gmail.com>
2021-04-18 01:19:33 -0400

    Create display_config_test.sh

---
+++ display_config_test.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+xrandr
+ctv 1920 1080
+xrand --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode Virtual1 1920x1080_60.00
+xrandr --output Virtual1 --mode 1920x1080_60.00
+
+exit
