79a6b18132ec73d1b32636edf662b829751add5c
sauron <sauron@mordor>
2021-04-24 22:28:06 -0400

    add quotation marks to git config file

--- local_git_config.sh
+++ local_git_config.sh
@@ -2,7 +2,7 @@
 
 sudo apt install git -y
 sudo git clone https://github.com/aswiehe/ScriptCanvas.git
-sudo git config --global user.name sauron
-sudo git config --global user.email sauron@mordor
+sudo git config --global user.name "sauron"
+sudo git config --global user.email "sauron@mordor"
 
 exit
