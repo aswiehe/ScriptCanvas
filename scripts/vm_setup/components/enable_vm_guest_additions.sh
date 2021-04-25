1d8977e1bf3cb330957536aff04633adaf171921
Avery Wiehe <averyswiehe@gmail.com>
2021-04-25 14:22:39 -0400

    yes option

--- scripts/vm_setup/components/enable_vm_guest_additions.sh
+++ scripts/vm_setup/components/enable_vm_guest_additions.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-sudo apt install virtualbox-guest-utils virtualbox-guest-dkms
+sudo apt install virtualbox-guest-utils virtualbox-guest-dkms -y
 sudo /media/sauron/VBox_GAs_6.1.18/VBoxLinuxAdditions.run
 
 exit
