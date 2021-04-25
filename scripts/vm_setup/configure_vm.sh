c5d95b6949b88a4693ede45d20734c670bdf3407
Avery Wiehe <averyswiehe@gmail.com>
2021-04-25 19:03:36 -0400

    Try writing output of bash scripts to their own log files to see what happens

--- scripts/vm_setup/configure_vm.sh
+++ scripts/vm_setup/configure_vm.sh
@@ -3,10 +3,10 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-bash ~/ScriptCanvas/scripts/vm_setup/components/enable_ssh.sh
-bash ~/ScriptCanvas/scripts/vm_setup/components/install_vlc.sh
-bash ~/ScriptCanvas/scripts/vm_setup/components/enable_vm_guest_additions.sh
-bash ~/ScriptCanvas/scripts/vm_setup/components/share_folders.sh
+bash ~/ScriptCanvas/scripts/vm_setup/components/enable_ssh.sh > /home/sauron/ScriptCanvas/logs/enable_ssh.log
+bash ~/ScriptCanvas/scripts/vm_setup/components/install_vlc.sh > /home/sauron/ScriptCanvas/logs/install_vlc.log
+bash ~/ScriptCanvas/scripts/vm_setup/components/enable_vm_guest_additions.sh > /home/sauron/ScriptCanvas/logs/enable_vm_guest_additions.log
+bash ~/ScriptCanvas/scripts/vm_setup/components/share_folders.sh > /home/sauron/ScriptCanvas/logs/ssh_folders.log
 sudo shutdown -r now
 
 exit
