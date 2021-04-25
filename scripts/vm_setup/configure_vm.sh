d632f7152091dee61795e0a8b4b9fc37886c7fdc
Avery Wiehe <averyswiehe@gmail.com>
2021-04-25 19:47:28 -0400

    Fix filename for shared folders log

--- scripts/vm_setup/configure_vm.sh
+++ scripts/vm_setup/configure_vm.sh
@@ -6,7 +6,7 @@ sudo apt upgrade -y
 bash ~/ScriptCanvas/scripts/vm_setup/components/enable_ssh.sh > /home/sauron/ScriptCanvas/logs/enable_ssh.log
 bash ~/ScriptCanvas/scripts/vm_setup/components/install_vlc.sh > /home/sauron/ScriptCanvas/logs/install_vlc.log
 bash ~/ScriptCanvas/scripts/vm_setup/components/enable_vm_guest_additions.sh > /home/sauron/ScriptCanvas/logs/enable_vm_guest_additions.log
-bash ~/ScriptCanvas/scripts/vm_setup/components/share_folders.sh > /home/sauron/ScriptCanvas/logs/ssh_folders.log
+bash ~/ScriptCanvas/scripts/vm_setup/components/share_folders.sh > /home/sauron/ScriptCanvas/logs/share_folders.log
 sudo shutdown -r now
 
 exit
