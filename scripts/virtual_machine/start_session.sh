6e5d2dd5d522bd85516e6cecabfe6a4e7fabb899
sauron <sauron@mordor>
2021-05-08 00:06:27 -0400

    Open github repo of this angular app

--- scripts/virtual_machine/start_session.sh
+++ scripts/virtual_machine/start_session.sh
@@ -2,9 +2,13 @@
 
 application_folder_name="Application"
 application_name="my-first-app"
+github_repo="my-first-app"
 
 cd ~/$application_folder_name
 cd $application_name
+
+# Open github repo for the application in default browser
+sensible-browser https://github.com/aswiehe/$github_repo &
 
 # Start server  in new terminal tab and open application in default browser
 gnome-terminal --tab -e 'ng serve --open'
