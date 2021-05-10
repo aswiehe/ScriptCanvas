9d1da1c3a420fbe93f1a27bcbc15128e3568e0b3
sauron <sauron@mordor>
2021-05-09 20:21:56 -0400

    filepath fix folder name

--- scripts/virtual_machine/configure_vm.sh
+++ scripts/virtual_machine/configure_vm.sh
@@ -3,10 +3,10 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-bash ~/ScriptCanvas/scripts/vm_setup/components/enable_ssh.sh > /home/sauron/ScriptCanvas/logs/enable_ssh.log
-bash ~/ScriptCanvas/scripts/vm_setup/components/install_vlc.sh > /home/sauron/ScriptCanvas/logs/install_vlc.log
-bash ~/ScriptCanvas/scripts/vm_setup/components/enable_vm_guest_additions.sh > /home/sauron/ScriptCanvas/logs/enable_vm_guest_additions.log
-bash ~/ScriptCanvas/scripts/vm_setup/components/share_folders.sh > /home/sauron/ScriptCanvas/logs/share_folders.log
+bash ~/ScriptCanvas/scripts/virtual_machine/components/enable_ssh.sh > /home/sauron/ScriptCanvas/logs/enable_ssh.log
+bash ~/ScriptCanvas/scripts/virtual_machine/components/install_vlc.sh > /home/sauron/ScriptCanvas/logs/install_vlc.log
+bash ~/ScriptCanvas/scripts/virtual_machine/components/enable_vm_guest_additions.sh > /home/sauron/ScriptCanvas/logs/enable_vm_guest_additions.log
+bash ~/ScriptCanvas/scripts/virtual_machine/components/share_folders.sh > /home/sauron/ScriptCanvas/logs/share_folders.log
 sudo shutdown -r now
 
 exit
